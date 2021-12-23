930c44f2d47ea4903a4e5edca8a75fb15dae3e81
taisei12232 <taisei12232@gmail.com>
2021-12-24 00:58:17 +0900

    gaj

--- main.py
+++ main.py
@@ -28,8 +28,7 @@ async def image(image_file: UploadFile = File(...)):
 
 def main() -> None:
     print("===== main() =====")
-    port = int(os.environ.get("PORT", 5000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True, workers=2)
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True, workers=1)
 
 
 if __name__ == "__main__":
